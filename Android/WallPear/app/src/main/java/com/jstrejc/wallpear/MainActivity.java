507d13e918a07ac92eafd22dd87aa782bf5354fd
Jan Strejc <jstrejc@outlook.com>
2017-01-07 20:03:41 +0100

    #6: Rename about activity to main activity

--- Android/WallPear/app/src/main/java/com/jstrejc/wallpear/MainActivity.java
+++ Android/WallPear/app/src/main/java/com/jstrejc/wallpear/MainActivity.java
@@ -3,11 +3,11 @@ package com.jstrejc.wallpear;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AboutActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.main_activity);
     }
 }
