7b88b34dd3fe067c4d0ac258a311e54d9ac43744
Jan Strejc <jstrejc@outlook.com>
2017-01-10 18:44:08 +0100

    #6 Added navigation drawer and appbar to main activity

--- Android/WallPear/app/src/main/java/com/jstrejc/wallpear/MainActivity.java
+++ Android/WallPear/app/src/main/java/com/jstrejc/wallpear/MainActivity.java
@@ -1,13 +1,38 @@
 package com.jstrejc.wallpear;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_activity);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Snackbar.make(findViewById(R.id.nav_view), item.getTitle(), Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+        return true;
     }
 }
